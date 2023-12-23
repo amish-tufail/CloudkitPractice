94644fc9f62f219972befceb042e3effdbdc2183
Amish <amish.tufail2002@gmail.com>
2023-12-23 18:48:48 +0500

    Initial Commit

---
+++ CloudkitPractice/CloudkitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudkitPracticeApp.swift
+//  CloudkitPractice
+//
+//  Created by Amish Tufail on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CloudkitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
